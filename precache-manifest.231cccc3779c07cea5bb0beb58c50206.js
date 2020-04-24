9a7d7cb35b821e6d6e6731dc68b49dfa0e4ef62b
Adam Davidson <adam26davidson@gmail.com>
2020-04-24 16:31:07 -0400

    Updates

---
+++ precache-manifest.231cccc3779c07cea5bb0beb58c50206.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8f4f246657db259526331b831c2e0f",
+    "url": "/wwww/index.html"
+  },
+  {
+    "revision": "07e60a2435257d5f6f5b",
+    "url": "/wwww/static/css/main.4732850e.chunk.css"
+  },
+  {
+    "revision": "9d5bba3a669a5af238ff",
+    "url": "/wwww/static/js/2.bb6b6040.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/wwww/static/js/2.bb6b6040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e60a2435257d5f6f5b",
+    "url": "/wwww/static/js/main.a9cc1a6e.chunk.js"
+  },
+  {
+    "revision": "f63037e474c35458cdac",
+    "url": "/wwww/static/js/runtime-main.af981ab6.js"
+  }
+]);
