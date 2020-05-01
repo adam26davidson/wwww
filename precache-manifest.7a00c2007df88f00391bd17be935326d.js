4c440cc211fb913d12355b652d43c2cdeeb1eaa0
Adam Davidson <adam26davidson@gmail.com>
2020-05-01 16:49:04 -0400

    Updates

--- precache-manifest.7a00c2007df88f00391bd17be935326d.js
+++ precache-manifest.7a00c2007df88f00391bd17be935326d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2e40288a4659e77abef19a5889517f00",
+    "revision": "937945aea1d7478a93e7dd7507284cb1",
     "url": "/wwww/index.html"
   },
   {
-    "revision": "7c30a6a8971ba51397dc",
+    "revision": "36d839f3f487e74fa95e",
     "url": "/wwww/static/css/main.0336bafa.chunk.css"
   },
   {
-    "revision": "d8d9c3ef5497b150c570",
-    "url": "/wwww/static/js/2.da50de45.chunk.js"
+    "revision": "d828bfbd812d5479450d",
+    "url": "/wwww/static/js/2.561f4b19.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/wwww/static/js/2.da50de45.chunk.js.LICENSE.txt"
+    "url": "/wwww/static/js/2.561f4b19.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7c30a6a8971ba51397dc",
-    "url": "/wwww/static/js/main.7669bf14.chunk.js"
+    "revision": "36d839f3f487e74fa95e",
+    "url": "/wwww/static/js/main.c8e6b5ae.chunk.js"
   },
   {
     "revision": "f63037e474c35458cdac",
